975563c4c5797d87a0a7ef7c300c4a3b404814e5
TYMtwilight <ore01100001binary@gmail.com>
2022-04-07 23:20:32 +0900

    🐛 プロフィール編集画面上の「キャンセル」ボタンのエラーを解決

--- src/components/ProfileForEnterprise/ProfileForEnterprise.tsx
+++ src/components/ProfileForEnterprise/ProfileForEnterprise.tsx
@@ -1,62 +1,69 @@
-import React, { useState } from "react";
-import { useAppSelector } from "../../app/hooks";
-import { selectUser } from "../../features/userSlice";
+import React, { useState, useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { selectUser, toggleIsNewUser } from "../../features/userSlice";
 import EditProfileForEnterprise from "../EditProfileForEnterprise/EditProfileForEnterprise";
 const ProfileForEnterprise: React.FC = () => {
   const user = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
   const [edit, setEdit] = useState<boolean>(false);
   const closeEdit: () => void = () => {
     setEdit(false);
   };
+  useEffect(() => {
+    if (user.isNewUser) {
+      setEdit(true);
+    }
+    return () => {
+      dispatch(toggleIsNewUser(false));
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
-      {(user.isNewUser || edit) && (
+      {edit && (
         <EditProfileForEnterprise
           onClick={() => {
             closeEdit();
           }}
         />
       )}
-      {!user.isNewUser && !edit && (
-        <>
-          <div id="top">
-            <img id="background" alt="背景画像" />
-            <img id="avatar" alt="アバター画像" />
-            {/* TODO >> ログインユーザーがプロフィール画面のユーザーと
+      <div id="top">
+        <img id="background" alt="背景画像" />
+        <img id="avatar" alt="アバター画像" />
+        {/* TODO >> ログインユーザーがプロフィール画面のユーザーと
                         異なる場合には、「フォロー」ボタンを表示するようにする  */}
-            <button
-              id="toEditProfile"
-              onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                e.preventDefault();
-                setEdit(true);
-              }}
-            >
-              編集する
-            </button>
-          </div>
-          <div id="profile">
-            <p id="introduction">説明文</p>
-            <button>さらに表示</button>
-            <div id="followerCount">
-              <p>フォロワー</p>
-              <p>人</p>
-            </div>
-            <div id="followeeCount">
-              <p>フォロー中</p>
-              <p>人</p>
-            </div>
-            <div>
-              <p id="owner"></p>
-            </div>
-            <div>
-              <p id="typeOfWork"></p>
-            </div>
-            <div>
-              <p id="address"></p>
-            </div>
-          </div>
-        </>
-      )}
+        <button
+          id="toEditProfile"
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+            e.preventDefault();
+            setEdit(true);
+          }}
+        >
+          編集する
+        </button>
+      </div>
+      <div id="profile">
+        <p id="introduction">説明文</p>
+        <button>さらに表示</button>
+        <div id="followerCount">
+          <p>フォロワー</p>
+          <p>人</p>
+        </div>
+        <div id="followeeCount">
+          <p>フォロー中</p>
+          <p>人</p>
+        </div>
+        <div>
+          <p id="owner"></p>
+        </div>
+        <div>
+          <p id="typeOfWork"></p>
+        </div>
+        <div>
+          <p id="address"></p>
+        </div>
+      </div>
     </div>
   );
 };
