3703aa51722228004100dd973cc4ed8d3e321be5
TYMtwilight <ore01100001binary@gmail.com>
2022-05-27 22:44:01 +0900

    ✨ URLの遷移後に対象ユーザーのプロフィールを表示する機能を実装

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Params, useParams } from "react-router-dom";
+import { db } from "../firebase";
+import {
+  collection,
+  CollectionReference,
+  DocumentData,
+  getDocs,
+  Query,
+  query,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+  where,
+} from "firebase/firestore";
 
 const Profile: React.VFC = () => {
-  let params:Readonly<Params<string>>= useParams();
-  return <h2>{params.username}のプロフィール画面</h2>;
+  const params: Readonly<Params<string>> = useParams();
+  const [username, setUsername] = useState<string>("");
+  const [displayName, setDisplayName] = useState<string>("");
+  const [photoURL, setPhotoURL] = useState<string>("");
+  const [userType, setUserType] = useState<
+    "businessUser" | "normalUser" | null
+  >(null);
+
+  const setProfile = async (isMounted: boolean) => {
+    setUsername(params.username!);
+    const usersRef: CollectionReference<DocumentData> = collection(db, "users");
+    const userQuery: Query<DocumentData> = query(
+      usersRef,
+      where("username", "==", username)
+    );
+    const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(userQuery);
+    querySnapshot.forEach((doc: QueryDocumentSnapshot) => {
+      if (isMounted) {
+        setDisplayName(doc.data().displayName);
+        setPhotoURL(doc.data().photoURL);
+        setUserType(doc.data().userType);
+        console.log(userType);
+      }
+    });
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+    setProfile(isMounted);
+    return () => {
+      isMounted = false;
+    };
+  });
+
+  return (
+    <div>
+      <div id="top">
+        <img
+          id="avatar"
+          src={photoURL ? photoURL : `${process.env.PUBLIC_URL}/noAvatar.png`}
+          alt="アバター画像"
+        />
+        <p id="username">{username}</p>
+        <p id="displayName">{displayName}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
