e58c68951bff12785857dbc27627b05e84351621
TYMtwilight <ore01100001binary@gmail.com>
2022-07-25 00:14:08 +0900

    ✨ 検索結果において投稿画像のほかの情報も表示できるようにしました#196

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,5 +1,6 @@
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useSearch } from "../hooks/useSearch";
+import { Favorite } from "@mui/icons-material";
 
 interface PostData {
   avatarURL: string;
@@ -45,7 +46,29 @@ const Search: React.VFC = () => {
       })}
       {posts.length > 0 &&
         posts.map((post: PostData) => {
-          return <img key={post.id} src={post.imageURL} alt={post.caption} />;
+          return (
+            <div key={post.id}>
+              <div>
+                <p id="displayName">{post.displayName}</p>
+                {/* <p id="timestamp">{post.timestamp}</p> */}
+                <Link to={`/${post.username}`}>
+                  <img id="avatarURL" src={post.avatarURL} alt="アバター画像" />
+                </Link>
+              </div>
+              <div>
+                <Link to={`/${post.id}`}>
+                  <img src={post.imageURL} alt={post.caption} />
+                </Link>
+                <div>
+                  <Favorite />
+                  <p id="likeCounts">0</p>
+                </div>
+              </div>
+              <div>
+                <p id="caption">{post.caption}</p>
+              </div>
+            </div>
+          );
         })}
     </div>
   );
