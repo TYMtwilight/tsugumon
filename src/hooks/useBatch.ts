5992325056a05f9cc829af33ec834c7154891e07
TYMtwilight <ore01100001binary@gmail.com>
2022-09-05 02:44:47 +0900

    👍 デモデータに登録する情報を追加しました

--- src/hooks/useBatch.ts
+++ src/hooks/useBatch.ts
@@ -23,6 +23,7 @@ import {
   QuerySnapshot,
   QueryDocumentSnapshot,
 } from "firebase/firestore";
+import {getRandomString} from "../functions/GetRandomString";
 
 interface Post {
   uid: string;
@@ -33,19 +34,6 @@ interface Post {
   caption: string;
   timestamp: FieldValue;
 }
-
-const getRandomString: () => string = () => {
-  const S: string =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const N: number = 16;
-  const randomValues: number[] = Array.from(
-    crypto.getRandomValues(new Uint32Array(N))
-  );
-  const randomString: string = randomValues
-    .map((n) => S[n % S.length])
-    .join("");
-  return randomString;
-};
 
 export const useBatch: (
   upload: boolean,
