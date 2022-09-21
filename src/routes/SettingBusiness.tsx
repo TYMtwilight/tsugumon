82b1edabc9fdad17ef7c576e0fabbb38c2a96729
TYMtwilight <ore01100001binary@gmail.com>
2022-09-22 01:26:28 +0900

    🎨 プロフィール設定画面のスタイリングをおこないました

--- src/routes/SettingBusiness.tsx
+++ src/routes/SettingBusiness.tsx
@@ -26,6 +26,10 @@ import {
 } from "firebase/storage";
 import { resizeImage } from "../functions/ResizeImage";
 import { checkUsername } from "../functions/CheckUsername";
+import { checkIsUnique } from "../functions/useCheckIsUnique";
+import ArrowBackRounded from "@mui/icons-material/ArrowBackIosNewRounded";
+import PhotoLibraryOutlined from "@mui/icons-material/PhotoLibraryOutlined";
+import PersonOutline from "@mui/icons-material/PersonOutline";
 import CloseRounded from "@mui/icons-material/CloseRounded";
 
 const SettingBusiness = () => {
@@ -41,8 +45,9 @@ const SettingBusiness = () => {
     uniqueCheck: true,
     input: "",
   });
+  const [displayName, setDisplayName] = useState<string>("");
+  const [isUnique, setIsUnique] = useState<boolean>(true);
   const address = useRef<HTMLInputElement>(null);
-  const displayName = useRef<HTMLInputElement>(null);
   const introduction = useRef<HTMLTextAreaElement>(null);
   const owner = useRef<HTMLInputElement>(null);
   const typeOfWork = useRef<HTMLInputElement>(null);
@@ -83,7 +88,7 @@ const SettingBusiness = () => {
       uniqueCheck: true,
       input: loginUser.username.slice(1),
     });
-    displayName.current!.value = loginUser.displayName;
+    setDisplayName(loginUser.displayName);
     introduction.current!.value = loginUser.introduction;
 
     getDoc(optionRef).then((optionSnap: DocumentSnapshot<DocumentData>) => {
@@ -147,22 +152,18 @@ const SettingBusiness = () => {
     } else {
       backgroundURL = loginUser.backgroundURL;
     }
-    updateProfile(auth.currentUser!, {
-      displayName: displayName.current!.value,
-      photoURL: avatarURL,
-    });
     dispatch(
       setUserProfile({
         avatarURL: avatarURL,
         backgroundURL: backgroundURL,
-        displayName: displayName.current!.value,
+        displayName: displayName,
         username: `@${username.input}`,
       })
     );
     updateDoc(loginUserRef, {
       avatarURL: avatarURL,
       backgroundURL: backgroundURL,
-      displayName: displayName.current!.value,
+      displayName: displayName,
       introduction: introduction.current!.value,
       username: `@${username.input}`,
     });
@@ -172,13 +173,17 @@ const SettingBusiness = () => {
       typeOfWork: typeOfWork.current!.value,
       username: `@${username.input}`,
     });
+    updateProfile(auth.currentUser!, {
+      displayName: displayName,
+      photoURL: avatarURL,
+    });
     getDocs(postsQuery)
       .then((posts: QuerySnapshot<DocumentData>) => {
         posts.forEach((post) => {
           const postRef = doc(db, "posts", post.id);
           updateDoc(postRef, {
             username: `@${username.input}`,
-            displayName: displayName.current!.value,
+            displayName: displayName,
             avatarURL: avatarURL,
           });
         });
@@ -204,135 +209,215 @@ const SettingBusiness = () => {
   }, [isFetched]);
 
   return (
-    <div>
-      <div>
+    <div className="bg-slage-100">
+      <div className="flex fixed justify-center items-center top-0 w-screen h-12 z-10 bg-slate-100">
         <button
+          className="absolute left-2"
           onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
             event.preventDefault();
             navigate(-1);
           }}
         >
-          戻る
+          <ArrowBackRounded />
         </button>
-        <p>プロフィールの編集</p>
+        <p className="w-40 mx-auto font-bold">プロフィールの編集</p>
       </div>
-      <form
-        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
-          event.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <input
-          id="avatarInput"
-          type="file"
-          accept="image/*"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            event.preventDefault();
-            onChangeImageHandler(event, "avatar");
-          }}
-          hidden
-        />
-        <label htmlFor="avatarInput">写真を変更</label>
-        <img
-          src={
-            avatarImage ? avatarImage : `${process.env.PUBLIC_URL}/noAvatar.png`
-          }
-          alt="アバター画像"
-        />
-        <button
-          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            event.preventDefault();
-            setAvatarImage("");
-          }}
-          disabled={!avatarImage}
-        >
-          <CloseRounded />
-        </button>
-        <input
-          id="backgroundInput"
-          type="file"
-          accept="image/*"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            event.preventDefault();
-            onChangeImageHandler(event, "background");
-          }}
-          hidden
-        />
-        <label htmlFor="backgroundInput">背景を選択</label>
-        <img
-          src={
-            backgroundImage
-              ? backgroundImage
-              : `${process.env.PUBLIC_URL}/noPhoto.png`
-          }
-          alt="背景画像"
-        />
-        <button
-          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            event.preventDefault();
-            setBackgroundImage("");
-          }}
-          disabled={!backgroundImage}
-        >
-          <CloseRounded />
-        </button>
-        <p>ユーザー名</p>
-        <input
-          type="text"
-          value={username.input}
-          onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
-            event.preventDefault();
-            const currentUsername: {
-              patternCheck: boolean;
-              uniqueCheck: boolean;
-              input: string;
-            } = await checkUsername(event.target.value);
-            if (currentUsername.input === loginUser.username.slice(1)) {
-              setUsername((prev) => {
-                return {
-                  input: currentUsername.input,
-                  uniqueCheck: true,
-                  patternCheck: currentUsername.patternCheck,
-                };
-              });
-            } else {
-              setUsername(currentUsername);
+      <div className="mt-12">
+        <div className="flex relative jutify-center items-center w-screen h-48">
+          <img
+            className="w-screen h-48 object-cover brightness-75"
+            src={
+              backgroundImage
+                ? backgroundImage
+                : `${process.env.PUBLIC_URL}/noPhoto.png`
             }
-          }}
-        />
-        <p>
-          {username.uniqueCheck === false &&
-            "既に使用されているユーザー名です。"}
-        </p>
-        <p>
-          {username.patternCheck === false &&
-            "入力できない文字が含まれいます。"}
-        </p>
-        <p>氏名</p>
-        <input
-          type="text"
-          ref={displayName}
-          defaultValue={loginUser.displayName}
-        />
-        <p>紹介文</p>
-        <textarea ref={introduction} defaultValue={loginUser.introduction} />
-        <p>事業主</p>
-        <input type="text" ref={owner} />
-        <p>職種</p>
-        <input type="text" ref={typeOfWork} />
-        <p>住所</p>
-        <input type="text" ref={address} />
-        <input
-          type="submit"
-          value="登録する"
-          disabled={
-            !displayName ||
-            !username.patternCheck ||
-            !username.uniqueCheck ||
-            !username.input
-          }
-        />
-      </form>
+            alt="背景画像"
+          />
+          <input
+            id="backgroundInput"
+            type="file"
+            accept="image/*"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              event.preventDefault();
+              onChangeImageHandler(event, "background");
+            }}
+            hidden
+          />
+          <label
+            className="flex absolute left-4 text-slate-100"
+            htmlFor="backgroundInput"
+          >
+            <PhotoLibraryOutlined fontSize="large" />
+            <p className="ml-4 leading-8">背景を選択</p>
+          </label>
+          <button
+            className="absolute right-4 bottom-4 p-2 rounded-full border border-slate-100 text-slate-100"
+            onClick={(
+              event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+            ) => {
+              event.preventDefault();
+              setBackgroundImage("");
+            }}
+            disabled={!backgroundImage}
+          >
+            <CloseRounded />
+          </button>
+        </div>
+        <div className="relative">
+          <input
+            id="avatarInput"
+            type="file"
+            accept="image/*"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              event.preventDefault();
+              onChangeImageHandler(event, "avatar");
+            }}
+            hidden
+          />
+          {avatarImage ? (
+            <img
+              className="-mt-8 ml-4 w-20 h-20 border-4 border-slate-100 rounded-full object-cover brightness-75"
+              src={
+                avatarImage
+                  ? avatarImage
+                  : `${process.env.PUBLIC_URL}/noAvatar.png`
+              }
+              alt="アバター画像"
+            />
+          ) : (
+            <div className="-mt-8 ml-4 w-20 h-20 border-4 border-slate-100 bg-slate-500 rounded-full" />
+          )}
+          <label
+            className="absolute flex justify-center items-center w-20 h-20 border-4 top-0 left-4 border-slate-100 rounded-full text-slate-100"
+            htmlFor="avatarInput"
+          >
+            <div className="box rounded-full">
+              <PersonOutline fontSize="large" />
+            </div>
+          </label>
+          <button
+            className="absolute bottom-0 left-24 p-1 border border-slate-500 rounded-full text-slate-500"
+            onClick={(
+              event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+            ) => {
+              event.preventDefault();
+              setAvatarImage("");
+            }}
+            disabled={!avatarImage}
+          >
+            <CloseRounded />
+          </button>
+        </div>
+        <div className="p-4">
+          <div className="mb-4">
+            <p className="text-sm text-slate-500">ユーザー名</p>
+            <input
+              className="h-8 w-full p-4  bg-slate-200 rounded-md"
+              type="text"
+              value={username.input}
+              onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
+                event.preventDefault();
+                const currentUsername: {
+                  patternCheck: boolean;
+                  uniqueCheck: boolean;
+                  input: string;
+                } = await checkUsername(event.target.value);
+                if (currentUsername.input === loginUser.username.slice(1)) {
+                  setUsername((prev) => {
+                    return {
+                      input: currentUsername.input,
+                      uniqueCheck: true,
+                      patternCheck: currentUsername.patternCheck,
+                    };
+                  });
+                } else {
+                  setUsername(currentUsername);
+                }
+              }}
+            />
+            <p className="text-sm text-slate-500">
+              {username.uniqueCheck === false &&
+                "既に使用されているユーザー名です。"}
+            </p>
+            <p className="text-sm text-slate-500">
+              {username.patternCheck === false &&
+                "入力できない文字が含まれいます。"}
+            </p>
+          </div>
+          <div className="mb-4">
+            <p className="text-sm text-slate-500">氏名</p>
+            <input
+              className="h-8 w-full p-4  bg-slate-200 rounded-md"
+              type="text"
+              value={displayName}
+              onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
+                setDisplayName(event.target.value);
+              }}
+              onBlur={async (
+                event: React.FocusEvent<HTMLInputElement, Element>
+              ) => {
+                event.preventDefault();
+                setIsUnique(
+                  await checkIsUnique(displayName, loginUser.displayName)
+                );
+              }}
+              required
+            />
+            <p className="text-sm text-slate-500">
+              {isUnique === false && "その氏名は既に使用されています。"}
+            </p>
+          </div>
+          <div className="mb-4">
+            <p className="text-sm text-slate-500">紹介文</p>
+            <textarea
+              className="w-full h-32 p-2 border-none bg-slate-200 rounded-md resize-none"
+              ref={introduction}
+              defaultValue={loginUser.introduction}
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-sm text-slate-500">事業主</p>
+            <input
+              className="h-8 w-full p-4  bg-slate-200 rounded-md"
+              type="text"
+              ref={owner}
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-sm text-slate-500">職種</p>
+            <input
+              className="h-8 w-full p-4  bg-slate-200 rounded-md"
+              type="text"
+              ref={typeOfWork}
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-sm text-slate-500">住所</p>
+            <input
+              className="h-8 w-full p-4  bg-slate-200 rounded-md"
+              type="text"
+              ref={address}
+            />
+          </div>
+        </div>
+        <div className="mb-8">
+          <button
+            className="block w-24 h-8 m-auto border rounded-full font-bold border-emerald-500 text-emerald-500 hover:border-none hover:bg-emerald-500 hover:text-slate-100 disabled:border-slate-400 disabled:text-slate-400 disabled:bg-slate-300"
+            onClick={(event) => {
+              event.preventDefault();
+              handleSubmit();
+            }}
+            disabled={
+              !isUnique ||
+              !username.patternCheck ||
+              !username.uniqueCheck ||
+              username.input === ""
+            }
+          >
+            登録する
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
