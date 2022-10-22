6a1e0c30cf9043f2b72bcec25a42a4e5d5ec8db7
TYMtwilight <ore01100001binary@gmail.com>
2022-10-22 18:18:23 +0900

    ✨ デモユーザーでのログインボタンを実装しました

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
-import { useDemo } from "../hooks/useDemo";
+// import { useDemo } from "../hooks/useDemo";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { checkPassword } from "../functions/CheckPassword";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
@@ -19,8 +19,8 @@ const UserAuthentication = () => {
     input: "",
   });
   const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [upload, setUpload] = useState<boolean>(false);
-  const progress: "wait" | "run" | "done" = useDemo(upload);
+  // const [upload, setUpload] = useState<boolean>(false);
+  // const progress: "wait" | "run" | "done" = useDemo(upload);
   const navigate = useNavigate();
 
   const login: (
@@ -29,37 +29,45 @@ const UserAuthentication = () => {
     event.preventDefault();
     if (email && password) {
       signInWithEmailAndPassword(auth, email, password.input).then(() => {
-        navigate("/", { replace:true });
+        navigate("/", { replace: true });
       });
     }
   };
 
-  useEffect(() => {
-    switch (progress) {
-      case "wait":
-        if (process.env.NODE_ENV === "development") {
-          console.log(`${progress}: アップロードの待機中`);
-          setUpload(false);
-        }
-        break;
-      case "run":
-        if (process.env.NODE_ENV === "development") {
-          console.log(`${progress}: アップロードの実行中`);
-        }
-        break;
-      case "done":
-        if (process.env.NODE_ENV === "development") {
-          console.log(`${progress}: アップロード完了`);
-          setUpload(false);
-        }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [progress]);
+  const demoLogin: () => void = () => {
+    signInWithEmailAndPassword(auth, "tsugumon@gmail.com", "tsugumon").then(
+      () => {
+        navigate("/", { replace: true });
+      }
+    );
+  };
+
+  // useEffect(() => {
+  //   switch (progress) {
+  //     case "wait":
+  //       if (process.env.NODE_ENV === "development") {
+  //         console.log(`${progress}: アップロードの待機中`);
+  //         setUpload(false);
+  //       }
+  //       break;
+  //     case "run":
+  //       if (process.env.NODE_ENV === "development") {
+  //         console.log(`${progress}: アップロードの実行中`);
+  //       }
+  //       break;
+  //     case "done":
+  //       if (process.env.NODE_ENV === "development") {
+  //         console.log(`${progress}: アップロード完了`);
+  //         setUpload(false);
+  //       }
+  //   }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [progress]);
 
   return (
     <div>
       <div>
-        <header className="flex h-40 justify-center items-center bg-hero bg-cover bg-bottom brightness-125">
+        <header className="flex h-44 mb-4 justify-center items-center bg-hero bg-cover bg-bottom brightness-125">
           <h1 className="font-kiwi text-2xl text-slate-100">つぐもん</h1>
         </header>
         <form className="p-4">
@@ -85,40 +93,42 @@ const UserAuthentication = () => {
             <label className="block" htmlFor="password">
               パスワード
             </label>
-            <div className="flex">
-              <input
-                className="block w-10/12 h-8 px-4 rounded-md outline-none bg-slate-200"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                data-testid="password"
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  event.preventDefault();
-                  setPassword(checkPassword(event));
-                }}
-                required
-              />
-              <button
-                className="ml-4"
-                onClick={(
-                  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-                ) => {
-                  e.preventDefault();
-                  setShowPassword(!showPassword);
-                }}
-              >
-                {showPassword ? (
-                  <Visibility color="primary" />
-                ) : (
-                  <VisibilityOff sx={{ color: blueGrey[500] }} />
+            <div className="flex flex-col pb-12">
+              <div className="flex flex-row">
+                <input
+                  className="block w-10/12 h-8 px-4 rounded-md outline-none bg-slate-200"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  data-testid="password"
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    event.preventDefault();
+                    setPassword(checkPassword(event));
+                  }}
+                  required
+                />
+                <button
+                  className="ml-4"
+                  onClick={(
+                    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                  ) => {
+                    e.preventDefault();
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? (
+                    <Visibility color="primary" />
+                  ) : (
+                    <VisibilityOff sx={{ color: blueGrey[500] }} />
+                  )}
+                </button>
+              </div>
+              <div className="h-8 mt-2">
+                {password.lengthCheck === false && (
+                  <p className="text-sm text-red-500 ">
+                    パスワードの入力文字数は8文字以上20文字以下です
+                  </p>
                 )}
-              </button>
-            </div>
-            <div className="h-12">
-              {password.lengthCheck === false && (
-                <p className="text-sm text-slate-400 ">
-                  パスワードは８文字以上入力してください
-                </p>
-              )}
+              </div>
             </div>
             <div className="flex justify-center">
               <button
@@ -145,7 +155,7 @@ const UserAuthentication = () => {
           </p>
         </div>
         <footer className="flex mt-16 justify-center">
-          <button
+          {/* <button
             className="w-40 h-8 rounded-md border hover:border-none border-slate-500 text-slate-500 hover:text-slate-100 font-bold hover:bg-slate-500"
             onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
               event.preventDefault();
@@ -153,6 +163,15 @@ const UserAuthentication = () => {
             }}
           >
             デモデータの登録
+          </button> */}
+          <button
+            className="w-48 h-8 rounded-md border hover:border-none border-slate-500 text-slate-500 hover:text-slate-100 font-bold hover:bg-slate-500"
+            onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+              event.preventDefault();
+              demoLogin();
+            }}
+          >
+            デモユーザーでログイン
           </button>
         </footer>
       </div>
