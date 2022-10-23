78c834a74828ce0a6ae165897e78c9c502e802ca
TYMtwilight <ore01100001binary@gmail.com>
2022-10-23 09:46:45 +0900

    🎨 SignUp画面のスタイリングをおこないました

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -152,9 +152,9 @@ const SignUp: React.VFC = () => {
   };
 
   return (
-    <div className="w-screen h-min-screen bg-slate-100">
-      <div className="flex fixed top-0">
-        <div className="flex relative  h-12 w-screen items-center bg-slate-100">
+    <div className="flex lg:justify-center md:justify-center h-screen bg-slate-100">
+      <div className="flex flex-col sm:w-screen md:w-1/2 lg:w-1/3 bg-white">
+        <div className="flex fixed sm:w-screen md:w-1/2 lg:w-1/3 h-12 justify-center items-center bg-white z-10">
           <button
             className="absolute left-2"
             onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
@@ -164,185 +164,187 @@ const SignUp: React.VFC = () => {
           >
             <ArrowBackRounded />
           </button>
-          <p className="w-24 mx-auto font-bold">ユーザー登録</p>
+          <p className="mx-auto font-bold">ユーザー登録</p>
         </div>
-      </div>
-      <div className="pt-16 p-4">
-        <div className="flex w-full justify-center">
-          <input
-            id="avatarInput"
-            type="file"
-            accept="image/*"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              event.preventDefault();
-              onChangeImageHandler(event);
-            }}
-            hidden
-          />
-          <div className="relative w-20 h-20 mb-8 rounded-full">
-            {avatarImage ? (
-              <img
-                className="w-20 h-20 rounded-full object-cover brightness-75"
-                src={avatarImage}
-                alt="アバター画像"
-              />
-            ) : (
-              <div className="w-20 h-20 bg-slate-500 rounded-full" />
-            )}
-            <label
-              htmlFor="avatarInput"
-              className="absolute flex justify-center items-center w-20 h-20 top-0 left-0 rounded-full text-slate-100 hover:cursor-pointer"
-            >
-              <PersonOutline fontSize="large" />
-            </label>
-            <button
-              className="absolute bottom-0 left-24 p-1 border border-slate-500 rounded-full text-slate-500"
-              onClick={(
-                event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-              ) => {
-                event.preventDefault();
-                setAvatarImage("");
-              }}
-              disabled={!avatarImage}
-            >
-              <CloseRounded />
-            </button>
-          </div>
-        </div>
-        <div></div>
-        <div className="mb-12">
-          <label htmlFor="displayName" className="text-sm text-slate-500">
-            会社名・個人名
-          </label>
-          <input
-            id="displayName"
-            className="h-8 w-full p-4 bg-slate-200 rounded-md"
-            type="text"
-            value={displayName}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setDisplayName(event.target.value);
-            }}
-            autoFocus
-            required
-          />
-        </div>
-        <div className="mb-12">
-          <label htmlFor="email" className="text-sm text-slate-500">
-            メールアドレス
-          </label>
-          <input
-            id="email"
-            className="h-8 w-full p-4 bg-slate-200 rounded-md"
-            name="textbox"
-            type="email"
-            value={email}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setEmail(event.target.value);
-            }}
-            placeholder="例) tsugumon@examle.com"
-            required
-          />
-        </div>
-        <div className="mb-8">
-          <label htmlFor="username" className="text-sm text-slate-500">
-            ユーザー名
-          </label>
-          <p className="my-2 text-xs">
-            入力できる文字はアルファベットの大文字、小文字、数字、_(アンダーバー)の4種類です。
-          </p>
-          <input
-            id="username"
-            className="h-8 w-full pl-4 py-4 pr-12 bg-slate-200 rounded-md"
-            type="text"
-            onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
-              event.preventDefault();
-              setUsername(await checkUsername(event.target.value));
-            }}
-          />
-          <div className="h-4">
-            <p className="my-2 text-sm text-red-500">
-              {username.uniqueCheck === false &&
-                "既に使用されているユーザー名です。"}
-            </p>
-            <p className="my-2 text-sm text-red-500">
-              {username.patternCheck === false &&
-                "入力できない文字が含まれいます。"}
-            </p>
-          </div>
-        </div>
-        <div className="mb-8">
-          <label htmlFor="password" className="text-sm text-slate-500">
-            パスワード
-          </label>
-          <p className="my-2 text-xs">
-            8文字以上20文字以下の文字を入力してください。
-          </p>
-          <p className="my-2 text-xs">
-            入力できる文字はアルファベットの大文字、小文字、数字、_(アンダーバー)の4種類です。
-          </p>
-          <div className="relative">
+
+        <div className="pt-16 p-4">
+          <div className="flex w-full justify-center">
             <input
-              id="password"
-              className="h-8 w-full pl-4 py-4 pr-12 bg-slate-200 rounded-md"
-              type={showPassword ? "text" : "password"}
+              id="avatarInput"
+              type="file"
+              accept="image/*"
               onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                 event.preventDefault();
-                setPassword(checkPassword(event));
+                onChangeImageHandler(event);
+              }}
+              hidden
+            />
+            <div className="relative w-20 h-20 mb-8 rounded-full">
+              {avatarImage ? (
+                <img
+                  className="w-20 h-20 rounded-full object-cover brightness-75"
+                  src={avatarImage}
+                  alt="アバター画像"
+                />
+              ) : (
+                <div className="w-20 h-20 bg-slate-500 rounded-full" />
+              )}
+              <label
+                htmlFor="avatarInput"
+                className="absolute flex justify-center items-center w-20 h-20 top-0 left-0 rounded-full text-slate-100 hover:cursor-pointer"
+              >
+                <PersonOutline fontSize="large" />
+              </label>
+              <button
+                className="absolute bottom-0 left-24 p-1 border border-slate-500 rounded-full text-slate-500"
+                onClick={(
+                  event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                ) => {
+                  event.preventDefault();
+                  setAvatarImage("");
+                }}
+                disabled={!avatarImage}
+              >
+                <CloseRounded />
+              </button>
+            </div>
+          </div>
+          <div></div>
+          <div className="mb-12">
+            <label htmlFor="displayName" className="text-sm text-slate-500">
+              会社名・個人名
+            </label>
+            <input
+              id="displayName"
+              className="h-8 w-full p-4 bg-slate-200 rounded-md"
+              type="text"
+              value={displayName}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setDisplayName(event.target.value);
+              }}
+              autoFocus
+              required
+            />
+          </div>
+          <div className="mb-12">
+            <label htmlFor="email" className="text-sm text-slate-500">
+              メールアドレス
+            </label>
+            <input
+              id="email"
+              className="h-8 w-full p-4 bg-slate-200 rounded-md"
+              name="textbox"
+              type="email"
+              value={email}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setEmail(event.target.value);
+              }}
+              placeholder="例) tsugumon@examle.com"
+              required
+            />
+          </div>
+          <div className="mb-8">
+            <label htmlFor="username" className="text-sm text-slate-500">
+              ユーザー名
+            </label>
+            <p className="my-2 text-xs">
+              入力できる文字はアルファベットの大文字、小文字、数字、_(アンダーバー)の4種類です。
+            </p>
+            <input
+              id="username"
+              className="h-8 w-full pl-4 py-4 pr-12 bg-slate-200 rounded-md"
+              type="text"
+              onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
+                event.preventDefault();
+                setUsername(await checkUsername(event.target.value));
               }}
             />
-            <div
-              className="absolute flex justify-center items-center w-8 h-8 top-0 right-2"
-              onClick={(
-                event: React.MouseEvent<HTMLDivElement, MouseEvent>
-              ) => {
-                event.preventDefault();
-                setShowPassword(!showPassword);
-              }}
-            >
-              {showPassword ? (
-                <p className="text-emerald-500">
-                  <Visibility />
+            <div className="h-4">
+              <p className="my-2 text-sm text-red-500">
+                {username.uniqueCheck === false &&
+                  "既に使用されているユーザー名です。"}
+              </p>
+              <p className="my-2 text-sm text-red-500">
+                {username.patternCheck === false &&
+                  "入力できない文字が含まれいます。"}
+              </p>
+            </div>
+          </div>
+          <div className="mb-8">
+            <label htmlFor="password" className="text-sm text-slate-500">
+              パスワード
+            </label>
+            <p className="my-2 text-xs">
+              8文字以上20文字以下の文字を入力してください。
+            </p>
+            <p className="my-2 text-xs">
+              入力できる文字はアルファベットの大文字、小文字、数字、_(アンダーバー)の4種類です。
+            </p>
+            <div className="relative">
+              <input
+                id="password"
+                className="h-8 w-full pl-4 py-4 pr-12 bg-slate-200 rounded-md"
+                type={showPassword ? "text" : "password"}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  event.preventDefault();
+                  setPassword(checkPassword(event));
+                }}
+              />
+              <div
+                className="absolute flex justify-center items-center w-8 h-8 top-0 right-2"
+                onClick={(
+                  event: React.MouseEvent<HTMLDivElement, MouseEvent>
+                ) => {
+                  event.preventDefault();
+                  setShowPassword(!showPassword);
+                }}
+              >
+                {showPassword ? (
+                  <p className="text-emerald-500">
+                    <Visibility />
+                  </p>
+                ) : (
+                  <p className="text-slate-500">
+                    <VisibilityOff />
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="h-12">
+              {!password.lengthCheck && (
+                <p className="my-2 text-sm text-red-500">
+                  8文字以上20文字以下で入力してください。
                 </p>
-              ) : (
-                <p className="text-slate-500">
-                  <VisibilityOff />
+              )}
+              {!password.patternCheck && (
+                <p className="my-2 text-sm text-red-500">
+                  入力できない文字が含まれています。
                 </p>
               )}
             </div>
           </div>
-          <div className="h-12">
-            {!password.lengthCheck && (
-              <p className="my-2 text-sm text-red-500">
-                8文字以上20文字以下で入力してください。
-              </p>
-            )}
-            {!password.patternCheck && (
-              <p className="my-2 text-sm text-red-500">
-                入力できない文字が含まれています。
-              </p>
-            )}
-          </div>
+          <button
+            className="block w-24 h-8 m-auto border rounded-full font-bold border-emerald-500 text-emerald-500 hover:border-none hover:bg-emerald-500 hover:text-slate-100 disabled:border-slate-400 disabled:text-slate-400 disabled:bg-slate-300"
+            onClick={(
+              event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+            ) => {
+              signUp(event);
+            }}
+            disabled={
+              !displayName ||
+              !email ||
+              !username.patternCheck ||
+              !username.uniqueCheck ||
+              !username.input ||
+              !password.lengthCheck ||
+              !password.patternCheck ||
+              !password.input
+            }
+          >
+            登録する
+          </button>
         </div>
-      </div>
-      <div>
-        <button
-          className="block w-24 h-8 m-auto border rounded-full font-bold border-emerald-500 text-emerald-500 hover:border-none hover:bg-emerald-500 hover:text-slate-100 disabled:border-slate-400 disabled:text-slate-400 disabled:bg-slate-300"
-          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            signUp(event);
-          }}
-          disabled={
-            !displayName ||
-            !email ||
-            !username.patternCheck ||
-            !username.uniqueCheck ||
-            !username.input ||
-            !password.lengthCheck ||
-            !password.patternCheck ||
-            !password.input
-          }
-        >
-          登録する
-        </button>
+        <div></div>
       </div>
     </div>
   );
