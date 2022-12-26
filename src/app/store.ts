6323c281d371cb1aebc690881e7b5dd9a2146138
TYMtwilight <ore01100001binary@gmail.com>
2022-12-27 01:01:28 +0900

    👍 Redux Persistを導入しました

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSliceReducer from "../features/userSlice";
+import { AnyAction, configureStore, Reducer } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, WebStorage } from "redux-persist";
+import storage from "redux-persist/es/storage";
+import userSliceReducer, { LoginUser } from "../features/userSlice";
+import { PersistPartial } from "redux-persist/es/persistReducer";
+import thunk from "redux-thunk";
 
+const persistConfig: { key: string; storage: WebStorage } = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer: Reducer<LoginUser & PersistPartial, AnyAction> =
+  persistReducer(persistConfig, userSliceReducer);
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export const store = configureStore({
-  reducer: {
-    user: userSliceReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
 });
-
+export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
