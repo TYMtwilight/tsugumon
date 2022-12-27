5b2ba80aca0d5764b17ee0f80f00f5f1d1d19511
TYMtwilight <ore01100001binary@gmail.com>
2022-12-27 09:47:05 +0900

    ♻️ store.tsとindex.tsxを修正しました#191

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,12 @@
 import { AnyAction, configureStore, Reducer } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, WebStorage } from "redux-persist";
-import storage from "redux-persist/es/storage";
+import storage from "redux-persist/lib/storage";
 import userSliceReducer, { LoginUser } from "../features/userSlice";
 import { PersistPartial } from "redux-persist/es/persistReducer";
 import thunk from "redux-thunk";
 
 const persistConfig: { key: string; storage: WebStorage } = {
-  key: "root",
+  key: "user",
   storage,
 };
 
@@ -16,7 +16,6 @@ const persistedReducer: Reducer<LoginUser & PersistPartial, AnyAction> =
 // eslint-disable-next-line import/no-anonymous-default-export
 export const store = configureStore({
   reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== "production",
   middleware: [thunk],
 });
 export const persistor = persistStore(store);
--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,6 @@ import DirectMessage from "./routes/DirectMessage";
 import { persistor, store } from "./app/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
-import * as serviceWorker from "./serviceWorker";
 import Login from "./routes/Login";
 
 ReactDOM.render(
@@ -55,8 +54,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
